bb471f821dcdcdf43d2abb3b7ee7587b235a1352
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-17 14:52:27 +0900

    Make sure user is logged in whilst creating profile interface

--- instagram/src/screens/Profile.js
+++ instagram/src/screens/Profile.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Text, FlatList, StyleSheet, View, Image } from 'react-native';
+import { f, auth, database, storage } from '../../config/config';
 
 class Profile extends React.Component {
+    state = {
+        loggedIn: false
+    }
+
+    componentDidMount() {
+        f.auth().onAuthStateChanged(user => {
+            if(user) {
+                this.setState({
+                    loggedIn: true
+                })
+            } else {
+                this.setState({
+                    loggedIn: false
+                })
+            }
+        })
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello, World!</Text>
+                {this.state.loggedIn ? (
+                    <Text>Hello, World!</Text>
+                ) : (
+                    <Text>You are not logged in</Text>
+                )}
+                
             </View>
         )
     }
