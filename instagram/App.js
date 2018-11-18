e3bff97b780ab07cdf395d90ec56e69e11c1178d
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-18 12:46:44 +0900

    added new components and modify navigations

--- instagram/App.js
+++ instagram/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
-import Feed from './src/screens/Feed.js';
-import Profile from './src/screens/Profile.js';
-import Upload from './src/screens/Upload.js';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import Feed from './src/screens/Feed';
+import Profile from './src/screens/Profile';
+import Upload from './src/screens/Upload';
+import UserProfile from './src/screens/UserProfile';
+import Comments from './src/screens/Comments';
 import { auth } from 'firebase';
 
-const MainStack = createBottomTabNavigator({
+const TabStack = createBottomTabNavigator({
   Feed: {screen: Feed},
   Upload: {screen: Upload},
   Profile: {screen: Profile}
+})
+
+const MainStack = createStackNavigator({
+  Home: { screen: TabStack },
+  User: { screen: UserProfile },
+  Comments: { screen: Comments }
+},{
+  initialRouteName: "Home", //Sets the default screen of the stack. Must match one of the keys in route configs.
+  mode: 'modal', //Make the screens slide in from the bottom which is a common iOS pattern. Only works on iOS, has no effect on Android.
+  headerMode: 'none' //Specifies how the header should be rendered: none - No header will be rendered.
 })
 
 export default class App extends React.Component {
