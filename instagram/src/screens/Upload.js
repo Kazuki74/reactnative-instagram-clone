b804c7b021be8be46dbd5ce73f10c6ed1f19f551
小林和希 <kobayashi.kazuki74@gmail.com>
2018-11-30 19:37:27 +0900

    fixed upload function

--- instagram/src/screens/Upload.js
+++ instagram/src/screens/Upload.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, FlatList, StyleSheet, View, Image, TouchableOpacity, TextInput, ActivityIndicator } from 'react-native';
+import { Text, FlatList, StyleSheet, View, Image, TouchableOpacity, TextInput, ActivityIndicator, Dime } from 'react-native';
 import { f, auth, database, storage } from '../../config/config';
 import { Permissions, ImagePicker } from 'expo';
 
@@ -74,7 +74,8 @@ class Upload extends React.Component {
         const regEx = /(?:\.([^.]+))?$/;
         const currentfFileType = regEx.exec(uri)[1];
         this.setState({
-            currentfFileType
+            currentfFileType,
+            uploading: true
         })
         // not working on expo sdk31
         // const response = await fetch(uri);
@@ -101,31 +102,41 @@ class Upload extends React.Component {
         uploadTask.on("state_changed", snap => {
             const progress = ((snap.bytesTransferred / snap.totalBytes) * 100 ).toFixed(0);
             console.log(`${progress}%`);
-            this.setState({
-                progress
+            this.setState({ progress });
             }, err => {
-                console.log(err)
+                console.log(err);
             }, () => {
+                this.setState({
+                    uploading: false
+                })
                 uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-                    console.log(downloadURL);
+                    console.log('File available at ' + downloadURL);
                 })
                 .catch(err => {
                     console.log(err);
                 })
-            })
-        })
+            }
+        )
+    }
+
+    uploadPublish = () => {
+        if(this.state.caption !== '') {
+            this.uploadImage(this.state.imageUri);
+        } else {
+            alert('Please enter a caption.');
+        }
     }
     
     render() {
         return (
             <View style={{flex: 1}}>
                 {this.state.loggedIn ? (
-                    <View style={styles.container}>
+                    <View style={{ flex: 1 }}>
+                        <View style={{ justifyContent: 'center', alignItems: 'center', height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightgrey', borderBottomWidth: 0.5}}>
+                            <Text>Upload</Text>
+                        </View>
                         { this.state.imageSelected === true ? (
                             <View style={{flex: 1}}>
-                                <View style={{ justifyContent: 'center', alignItems: 'center', height: 70, paddingTop: 30, backgroundColor: 'white', borderColor: 'lightgrey', borderBottomWidth: 0.5}}>
-                                    <Text>Upload</Text>
-                                </View>
                                 <View style={{ padding: 5 }}>
                                     <Text style={{marginTop: 5}}>Caption...</Text>
                                     <TextInput 
@@ -147,12 +158,37 @@ class Upload extends React.Component {
                                         }}
                                     />
                                 </View>
+                                <TouchableOpacity
+                                    onPress={() => { this.uploadPublish() }}
+                                    style={{flex: 1, alignItems: 'center'}}
+                                >
+                                    <Text>Publish</Text>
+                                </TouchableOpacity>
+                                { this.state.uploading === true ? (
+                                    <View style={{ marginTop: 10 }}>
+                                        { this.state.progress !== 100 ? (
+                                            <ActivityIndicator size="small" color="blue"/>
+                                        ) : 
+                                            <Text>
+                                                Processing...
+                                            </Text>
+                                        }
+                                    </View>
+                                ) : (
+                                    <View></View>
+                                )}
+                                <Image 
+                                    source={{ uri: this.state.imageUri }}
+                                    style={{ marginTop: 10, resizeMode: 'cover', width: "100%", height: 275 }}
+                                />
                             </View>
                         ) : (
-                            <TouchableOpacity onPress={() => this.findNewImage()}>
-                                <Text style={{fontSize: 28, paddingBottom: 5}}>Upload</Text>
-                                <Text>Select Photo</Text>
-                            </TouchableOpacity>
+                            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                                <TouchableOpacity onPress={() => this.findNewImage()}>
+                                    <Text style={{fontSize: 28, paddingBottom: 5}}>Upload</Text>
+                                    <Text>Select Photo</Text>
+                                </TouchableOpacity>
+                            </View>
                         )}
                     </View>
                 ) : (
